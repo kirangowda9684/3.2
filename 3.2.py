b9f1040b96111a2733e9818d3c27a82d06530815
kirangowda9684 <69901085+kirangowda9684@users.noreply.github.com>
2022-12-23 16:23:45 +0530

    Update 3.2.py

--- 3.2.py
+++ 3.2.py
@@ -1,10 +1,9 @@
 def test(n):    
     return [n + 2 * i for i in range(n)]   
         
-        #  there is a for loop // what is the range 0 1 2 
-        # when n = 0
+        #  there is a for loop // the range of i is 0 1 2 
+        # when n = 2
         # i = 0 // 2 + 2 * 0
-        # when n = 1
         # i = 1 // 2 + 2 * 1
 
 n = 2
